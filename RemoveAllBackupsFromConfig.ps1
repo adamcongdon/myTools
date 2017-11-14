334b0c67d02e393fd36b2c297ea2af2d018fef87
Adam-v9server <cac8907@gmail.com>
2017-11-14 15:52:00 -0500

    Does what it says
    Skips replicas. Removed Backups 1by1 in a loop

---
+++ RemoveAllBackupsFromConfig.ps1
@@ -0,0 +1,22 @@
+﻿# by Adam.Congdon
+# this tool should remove all backups from configuration database but skip replicas
+# see here for more detail: https://helpcenter.veeam.com/docs/backup/powershell/remove-vbrrestorepoint.html?ver=95
+# Use at your own risk - I tested with success in my lab.
+
+Add-PSSnapin VeeamPSSnapin
+$backup = Get-VBRBackup
+
+$restorepoints = Get-VBRRestorePoint
+foreach($point in $restorepoints)
+{
+    #write-host $point.backupid
+    if ($backup.id -notcontains $point.backupid)
+    {
+        
+    }
+    else
+    {
+        Remove-VBRRestorePoint $point -Confirm:$false
+    }
+}
+
