a559505b93ad075420a2451b1341c58d97634c17
Adam-v9server <cac8907@gmail.com>
2017-11-14 17:42:34 -0500

    Does what it says

---
+++ RemoveUnmappedBackup.ps1
@@ -0,0 +1,26 @@
+﻿# by Adam.Congdon
+# see here for more detail: https://helpcenter.veeam.com/docs/backup/powershell/
+# Use at your own risk - I tested with success in my lab.
+
+# WHAT DO?
+# Removed all un-mapped backups from configuration. You can change the flag to delete from DISK if you so choose
+
+Add-PSSnapin VeeamPSSnapin
+$backups = Get-VBRBackup
+$jobs = Get-VBRJob
+
+foreach ($backup in $backups)
+{
+    if($backup.jobid -notin $job.id)
+    {
+        try
+        {
+            Write-Host "Removing backup: " $backup.Name -ForegroundColor Green
+            Remove-VBRBackup $backup -Confirm:$false -FromDisk:$false > $null
+        }
+        catch
+        {
+            Write-Host "Failed to remove backup, please try manually." -ForegroundColor Red
+        }
+    }
+}
