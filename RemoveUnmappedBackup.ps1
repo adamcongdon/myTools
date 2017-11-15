4f0af462ddbe26fbbde1a89783553184e7ebbc51
Adam-v9server <cac8907@gmail.com>
2017-11-15 09:32:47 -0500

    Connect options added
    Added requirement to connect to VBR server + tested to ensure only un-mapped are removed from config.
    
    Flip "-FromDisk:$false" to $true if you wish to delete from disk.

--- RemoveUnmappedBackup.ps1
+++ RemoveUnmappedBackup.ps1
@@ -6,12 +6,26 @@
 # Removed all un-mapped backups from configuration. You can change the flag to delete from DISK if you so choose
 
 Add-PSSnapin VeeamPSSnapin
+
+    $vbrServer = Read-Host -Prompt "Please Enter Veeam Backup Server you wish to connect or use localhost to connect locally"
+    Try
+        {
+            Disconnect-VBRServer -ErrorAction SilentlyContinue #This is to ensure connections are cleared to prevent failure/duplicate connections to VBRServer
+            Connect-VBRServer -Server $vbrServer -ErrorAction Stop
+        }
+    Catch
+        {
+            Write-Host $Error[0] -ForegroundColor Red
+            exit
+        }
+
+
 $backups = Get-VBRBackup
 $jobs = Get-VBRJob
 
 foreach ($backup in $backups)
 {
-    if($backup.jobid -notin $job.id)
+    if($backup.jobid -notin $jobs.id)
     {
         try
         {
