29b3417431d5c310e4171067192037abbb6d8052
AdamC <adam.congdon@veeam.com>
2018-06-21 10:40:41 -0400

    rough start

---
+++ FlrSshHelper.ps1
@@ -0,0 +1,20 @@
+﻿<#     
+.NOTES 
+#=========================================================================== 
+# Script: 
+# Author: AdamC
+# Purpose: Use SSH to gather helpful info from Linux FLR Appliance
+#=========================================================================== 
+.DESCRIPTION 
+
+#> 
+
+Add-PSSnapin VeeamPSSnapin;
+Add-Type -Path "C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.SSH.dll";
+Add-Type -Path "C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Core.dll";
+
+#$flrAppliance = Read-Host("Enter the IP assigned to the FLR appliance: ")
+$flrAppliance = ""
+
+[Veeam.Backup.SSH.CSshConnection.Protocol]::CreateCachedConnection()
+# Veeam.Backup.SSH.CSshConnection.Protocol 
